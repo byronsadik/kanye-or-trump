e12c2d56dbd1b8a4a6eaba109169648ed17f2471
Byron <byron@attoconsulting.com>
2019-11-09 21:41:03 -0500

    got it working with vanilla JS, itme to adapt it to React

--- src/kanyeortrump.js
+++ src/kanyeortrump.js
@@ -1,36 +1,6 @@
 export function yeezyOrTrump(){
-    var yeezy = 'https://api.kanye.rest?format=json',
-        trump = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random',
-        yeezyOrTrump =  (Math.round((Math.random() * 1) + 0) === 0),
-        calloutURL,
-        responseMessage
-      ;
-
-    calloutURL = yeezyOrTrump ? yeezy : trump;
-
-    console.log(calloutURL);
-
-    fetch(calloutURL)
-        .then((res) => { return res.json() })
-        .then((data) => {
-
-            if (yeezyOrTrump) {
-                yeezyMessage(data);
-            } else {
-                trumpMessage(data);
-            }
-
-            console.log(responseMessage);
-
-        });
 
 
-    function trumpMessage(data){
-        responseMessage = data.message;
-    }
 
-    function yeezyMessage(data){
-        responseMessage = data.quote;
-    }
-    
+
 }
